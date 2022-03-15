a2366f195bcb2279ee74e65b5f09ad62ff971350
manlikeghost <ekpoabasiama@gmail.com>
2022-03-15 15:21:23 +0100

    frontend set-up and landing page setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,20 @@
-import React, { Fragment } from 'react'
-
-
-
+import React, { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/gen-layout/Header';
+import Footer from './components/gen-layout/Footer';
 
 const App = () => {
-  return (
-    <Fragment>
-      <h1>Ghost shop is here</h1>
-    </Fragment>
-  );
-}
+	return (
+		<Fragment>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Ghost shop is here</h1>
+				</Container>
+			</main>
+			<Footer />
+		</Fragment>
+	);
+};
 
 export default App;
