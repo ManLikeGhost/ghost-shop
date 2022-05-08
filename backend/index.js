c6e35ff42769d307c46f5d7898e9d0360787ca63
manlikeghost <ekpoabasiama@gmail.com>
2022-05-08 01:15:45 +0100

    basic backend setup

--- backend/index.js
+++ backend/index.js
@@ -3,16 +3,23 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 // import products from './data/products.js'
 import productRoutes from './routes/productsRoute.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB()
 const app = express();
+
+
 
 app.get('/', (req,res) => {
 	res.send('API is running...')
 } ) 
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
