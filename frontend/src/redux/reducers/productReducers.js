bfa74f4c019b288dfc42afe513b2256172cce644
manlikeghost <ekpoabasiama@gmail.com>
2022-05-13 23:09:54 +0100

    rendered product list from backend with redux

---
+++ frontend/src/redux/reducers/productReducers.js
@@ -0,0 +1,34 @@
+import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL} from '../actions/types'
+
+const initialState = {
+  loading: null,
+  products: [],
+  error: []
+}
+
+
+export const productListReducer = ( state  = initialState, action ) => {
+  const { payload, type } = action
+  
+  switch ( type ) {
+    case PRODUCT_LIST_REQUEST:
+      return {
+        loading: true,
+        products:[]
+      };
+    
+    case PRODUCT_LIST_SUCCESS:
+      return {
+        loading: false,
+        products: payload
+      };
+    
+    case PRODUCT_LIST_FAIL:
+      return {
+        loading: false,
+        error: payload
+      };
+    default:
+      return state;
+  }
+} 
