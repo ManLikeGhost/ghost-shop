14afc190303e33a0baa335f9070c1c2cffe43105
manlikeghost <ekpoabasiama@gmail.com>
2022-05-30 15:39:21 +0100

    setup cartscreen and route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/shared-layout/Header';
 import Footer from './components/shared-layout/Footer';
 import HomeScreens from './screens/HomeScreens';
 import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 
 
 const App = () => {
@@ -15,7 +16,8 @@ const App = () => {
 				<main className='py-3'>
 					<Container>
 						<Route exact path='/' component={HomeScreens} />
-						<Route exact path='/product/:id' component={ProductScreen}/>
+						<Route exact path='/product/:id' component={ProductScreen} />
+						<Route exact path='/cart/:id?' component={CartScreen}/>
 					</Container>
 				</main>
 				<Footer />
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import {
 	Row,
@@ -8,6 +8,7 @@ import {
 	Card,
 	Button,
 	ListGroupItem,
+	FormControl,
 } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import { useDispatch, useSelector } from 'react-redux';
@@ -16,82 +17,106 @@ import { listProductDetail } from '../redux/actions/productActions';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 
-const ProductScreen = ( { match } ) => {
-	const dispatch = useDispatch()
-	const productDetails = useSelector((state)=>state.productDetails)
-	const{loading, error, product} = productDetails 
-	useEffect( () => {
-		dispatch(listProductDetail(match.params.id))
-	
-		
-	}, [dispatch,match,])
+const ProductScreen = ({ history, match }) => {
+	const [qty, setQty] = useState(0);
+	const dispatch = useDispatch();
+	const productDetails = useSelector((state) => state.productDetails);
+	const { loading, error, product } = productDetails;
+	useEffect(() => {
+		dispatch(listProductDetail(match.params.id));
+	}, [dispatch, match]);
 	// const product = {}
 	// const product = products.find((p) => p._id === match.params.id);
+	const addToCartHandler = () => {
+		history.push(`/cart/${match.params.id}?qty=${qty}`)
+	}
+	
 	return (
+
 		<>
 			<Link className='btn btn-light my-3' to='/'>
 				Go Back
 			</Link>
-			{
-				loading ? (
-				<Loader/>
+			{loading ? (
+				<Loader />
 			) : error ? (
-					<Message variant='danger'>{error}</Message> 
-			) : (<Row>
-				<Col md={6}>
-					<Image src={product.image} alt={product.name} fluid />
-				</Col>
-				<Col md={3}>
-					<ListGroup variant='flush'>
-						<ListGroupItem>
-							<h4>{product.name}</h4>
-						</ListGroupItem>
-						<ListGroupItem>
-							<Rating
-								value={product.rating}
-								text={`${product.numReviews} reviews`}
-							/>
-						</ListGroupItem>
-						<ListGroupItem>price: ${product.price}</ListGroupItem>
-						<ListGroupItem>Description: {product.description}</ListGroupItem>
-					</ListGroup>
-				</Col>
-				<Col md={3}>
-					<Card>
+				<Message variant='danger'>{error}</Message>
+			) : (
+				<Row>
+					<Col md={6}>
+						<Image src={product.image} alt={product.name} fluid />
+					</Col>
+					<Col md={3}>
 						<ListGroup variant='flush'>
 							<ListGroupItem>
-								<Row>
-									<Col>Price:</Col>
-									<Col>
-										<strong>${product.price}</strong>
-									</Col>
-								</Row>
+								<h4>{product.name}</h4>
 							</ListGroupItem>
 							<ListGroupItem>
-								<Row>
-									<Col>Status:</Col>
-									<Col>
-										{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-									</Col>
-								</Row>
+								<Rating
+									value={product.rating}
+									text={`${product.numReviews} reviews`}
+								/>
 							</ListGroupItem>
-							<ListGroupItem className='d-flex justify-content-center align-items-center'>
-								<Button
-									className='btn-block'
-									type='button'
-									disabled={product.countInStock === 0}
-								>
-									Add To Cart
-								</Button>
-							</ListGroupItem>
+							<ListGroupItem>price: ${product.price}</ListGroupItem>
+							<ListGroupItem>Description: {product.description}</ListGroupItem>
 						</ListGroup>
-					{/* comment */}
-					</Card>
-				</Col>
-			</Row>
-	)
-			}
-			
+					</Col>
+					<Col md={3}>
+						<Card>
+							<ListGroup variant='flush'>
+								<ListGroupItem>
+									<Row>
+										<Col>Price:</Col>
+										<Col>
+											<strong>${product.price}</strong>
+										</Col>
+									</Row>
+								</ListGroupItem>
+								<ListGroupItem>
+									<Row>
+										<Col>Status:</Col>
+										<Col>
+											{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+										</Col>
+									</Row>
+								</ListGroupItem>
+
+								{product.countInStock > 0 && (
+									<ListGroupItem>
+										<Row>
+											<Col>Quantity:</Col>
+											<Col>
+												<FormControl
+													as='select'
+													value={qty}
+													onChange={(e) => setQty(e.target.value)}
+												>
+													{[...Array(product.countInStock).keys()].map((x) => (
+														<option key={x + 1} value={x + 1}>
+															{x + 1}
+														</option>
+													))}
+												</FormControl>
+											</Col>
+										</Row>
+									</ListGroupItem>
+								)}
+								<ListGroupItem className='d-flex justify-content-center align-items-center'>
+									<Button
+										onClick={addToCartHandler}
+										className='btn-block'
+										type='button'
+										disabled={product.countInStock === 0}
+									>
+										Add To Cart
+									</Button>
+								</ListGroupItem>
+							</ListGroup>
+							{/* comment */}
+						</Card>
+					</Col>
+				</Row>
+			)}
 		</>
 	);
 };
