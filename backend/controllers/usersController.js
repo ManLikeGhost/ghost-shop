463132f426469463b41257e2a3de411844e37256
manlikeghost <ekpoabasiama@gmail.com>
2022-07-10 04:00:43 +0100

    user controller created

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,14 @@
+import User from '../models/userModels.js';
+import asyncHandler from 'express-async-handler';
+
+
+
+
+//@desc Authenticate the user & get token
+//@route POST /api/users/login
+//@access Public
+
+const authUser = asyncHandler(async(req, res) => {
+  
+})
+
