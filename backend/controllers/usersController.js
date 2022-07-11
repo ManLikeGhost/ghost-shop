1a90fe134a1d92c660c9d6e57512a1b5908a7db9
manlikeghost <ekpoabasiama@gmail.com>
2022-07-11 23:58:04 +0100

    finializning the touches of the backend auth

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -3,6 +3,47 @@ import asyncHandler from 'express-async-handler';
 import generateToken from '../utils/generateToken.js'
 
 
+//@desc Register a new user
+//@route POST /api/users
+//@access Public
+
+const registerUser = asyncHandler( async ( req, res ) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne( {
+    email
+  } )
+
+  if (userExists) {
+    res.status( 400 )
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create( {
+    name,
+    email,
+    password
+  } )
+  
+  if ( user ) {
+    res.status( 201 ).json( {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken( user._id )
+    })
+  } else {
+    res.status( 400 )
+    throw new Error('Invalid user data')
+  }
+
+} )
+
+
+
+
+
 
 //@desc Authenticate the user & get token
 //@route POST /api/users/login
@@ -54,4 +95,4 @@ const fetchUserProfile = asyncHandler( async ( req, res ) => {
 } )
 
 
-export { authUser, fetchUserProfile }
+export { authUser, fetchUserProfile, registerUser }
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,10 @@
 const router = express.Router();
 import express from 'express';
-import { authUser, fetchUserProfile } from '../controllers/usersController.js'
+import { authUser, fetchUserProfile, registerUser } from '../controllers/usersController.js'
 import { protect } from '../middleware/authMIdleware.js'
 
 
-
+router.route( '/' ).post( registerUser );
 router.post( '/login', authUser );
 router.route( '/profile' ).get( protect, fetchUserProfile );
 
